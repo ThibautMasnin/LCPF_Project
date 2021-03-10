19a223cb22820a2bdda8a4c996aa8357ec151959
Thibaut <thibautmasnin@outlook.fr>
2021-03-10 02:42:16 +0100

    Episode 1 done

--- Cha4.hs
+++ Cha4.hs
@@ -19,5 +19,5 @@ constraint = And (Eqv (Var "p1") (Not (Var "t1"))) (Eqv (Var "p2") (Not (Var "t2
 reglement :: Formula
 reglement = And (And (Imp (Var "p1") door1) (Imp (Var "t1") (Not door1))) (And (Imp (Var "t2") door2) (Imp (Var "p2") (Not door2)))
 
-challenge5 :: Formula
-challenge5 = And constraint reglement
+challenge4 :: Formula
+challenge4 = And constraint reglement
--- Cha6.hs
+++ Cha6.hs
@@ -11,10 +11,10 @@ door3 :: Formula
 door3 = Var "t2"
 
 constraint :: Formula
-constraint = And (Eqv (Var "p1") (Not (Var "t1"))) (Eqv (Var "p2") (Not (Var "t2")))
+constraint = And (And (Eqv (Var "p1") (Not (Var "t1"))) (Imp (Var "p1") (And (Var "t2") (Var "t3")))) (And (And (Eqv (Var "p2") (Not (Var "t2"))) (Imp (Var "p2") (And (Var "t1") (Var "t3")))) (And (Eqv (Var "p3") (Not (Var "t3"))) (Imp (Var "p3") (And (Var "t1") (Var "t2")))))
 
 reglement :: Formula
-reglement =
+reglement = Or (And door1 (And (Not door2) (Not door3))) (Or (And door2 (And (Not door1) (Not door3))) (And door3 (And (Not door1) (Not door2))))
 
 challenge6 :: Formula 
 challenge6 = And reglement constraint
