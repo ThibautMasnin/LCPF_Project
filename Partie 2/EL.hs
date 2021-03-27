5bc8fdd61c80443ff3ec97a3848e28378ecca216
Neonetix <antoine@msn.com>
2021-03-27 17:32:24 +0100

    question 3

--- Partie 2/EL.hs
+++ Partie 2/EL.hs
@@ -6,3 +6,15 @@ type Prop = [Char]
 type Agent = [Char]
 type World = Int
 type EpiState = (Prop -> [World], Agent -> World -> [World], World)
+
+data EpiFormula = T
+    | F
+    | Var String
+    | Not EpiFormula
+    | And EpiFormula EpiFormula
+    | Or EpiFormula EpiFormula
+    | Imp EpiFormula EpiFormula
+    | Eqv EpiFormula EpiFormula
+    | Knows String EpiFormula
+    | After EpiFormula EpiFormula
+    deriving (Eq, Show)
