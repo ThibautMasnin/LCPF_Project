88fad2c71a4ab61056468a525cbd3c895efa0196
Thibaut <thibautmasnin@outlook.fr>
2021-03-27 18:41:04 +0100

    Q5

--- Partie 2/EL.hs
+++ Partie 2/EL.hs
@@ -22,13 +22,12 @@ data EpiFormula = T
 epiSat :: EpiState -> EpiFormula -> Bool
 epiSat _ T = True
 epiSat _ F = False
-epiSat s (Var p) = p `elem` s
+epiSat (interp, _, w) (Var p) = interp p `elem` w
 epiSat s (Not phi) = not (epiSat s phi)
 epiSat s (And phi psi) = epiSat s phi && epiSat s psi
 epiSat s (Or phi psi) = epiSat s phi || epiSat s psi
 epiSat s (Imp phi psi) = not(epiSat s phi) || epiSat s psi
 epiSat s (Eqv phi psi) = epiSat s (Imp phi psi) && epiSat s (Imp psi phi)
-epiSat (interp, _, w) (Var p) = interp p `elem` w
 epiSat (interp, indis, w) (Knows a phi) = all (\x->(epiSat (interp, indis, x) phi)) (indis a w)
 epiSat s (After phi psi) = 
 
